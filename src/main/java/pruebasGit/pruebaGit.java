d4cb9a8b7c95ca955a270964c1923d0486059680
Pablo HZ <pablohuz@gmail.com>
2021-03-06 20:08:30 +0100

    cuarto commit

--- src/main/java/pruebasGit/pruebaGit.java
+++ src/main/java/pruebasGit/pruebaGit.java
@@ -3,6 +3,6 @@ package pruebasGit;
 public class pruebaGit {
 
     public static void main(String[] args) {
-        System.out.println(Sumador.sumar(15f, 4f));
+        System.out.println(Sumador.sumar(15f, 4fo));
     }
 }
